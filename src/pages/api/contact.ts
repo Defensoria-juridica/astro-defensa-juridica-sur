a92b6e894802f335c7864f5410fd9ed5ad5e8a9d
Defenseria juridica <defensoria.surpm@gmail.com>
2026-03-20 16:40:45 -0300

    comilla de mas

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -5,7 +5,7 @@ import sgMail from "@sendgrid/mail";
 const SENDGRID_API_KEY = import.meta.env.SENDGRID_API_KEY;
 const FROM_EMAIL = import.meta.env.SENDGRID_FROM_EMAIL || "noreply@defensajuridicasur.cl";
 const TO_EMAIL = import.meta.env.SENDGRID_TO_EMAIL || "juridicasurdefensa@gmail.com";
-";
+
 
 if (SENDGRID_API_KEY) {
     sgMail.setApiKey(SENDGRID_API_KEY);
