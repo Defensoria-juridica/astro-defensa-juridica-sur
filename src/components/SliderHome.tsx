ec43a898eab2910347971ecdfeefee35a15efd66
Defenseria juridica <defensoria.surpm@gmail.com>
2026-04-06 23:08:48 -0400

    Fix slider images tamaño

--- src/components/SliderHome.tsx
+++ src/components/SliderHome.tsx
@@ -235,7 +235,7 @@ export const SliderHome: React.FC<SliderHomeProps> = ({
                              alt={currentSlide.title}
                              className={
                              currentSlide.id === 1
-                             ?"w-full h-full object-cover object-[center_15%]"
+                             ?"w-full h-full object-cover object-[center_50%]"
                              :"w-full h-full object-cover object-[center_30%]"
                             }
                             />
