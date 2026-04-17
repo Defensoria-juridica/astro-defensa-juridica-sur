48e740a51a7bc2dff0f19562310d5aa7a89fce2b
Defenseria juridica <defensoria.surpm@gmail.com>
2026-04-16 23:43:53 -0400

    portada mas abajo

--- src/components/SliderHome.tsx
+++ src/components/SliderHome.tsx
@@ -199,7 +199,7 @@ export const SliderHome: React.FC<SliderHomeProps> = ({
 
     return (
         <section
-            className={`relative w-full h-[105vh] overflow-hidden ${className}`}
+            className={`relative w-full h-[115vh] overflow-hidden ${className}`}
             onMouseEnter={() => setIsPaused(true)}
             onMouseLeave={() => setIsPaused(false)}
             onTouchStart={onTouchStart}
