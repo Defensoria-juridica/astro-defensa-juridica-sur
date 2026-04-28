166f684ed9a6c2d64c0b3536883ec78fd3d45f54
Defenseria juridica <defensoria.surpm@gmail.com>
2026-04-27 22:02:48 -0400

    cambio de imagen

--- src/components/SliderHome.tsx
+++ src/components/SliderHome.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { AnimatePresence, motion } from "motion/react";
 
 // Assets de imágenes optimizadas
-import slider01 from "../assets/images/portadahome1.webp";
+import slider01 from "../assets/images/portada11.webp";
 import slider02 from "../assets/images/portadahomesec.webp";
 import slider02mobile from "../assets/images/slider-02-mobile.webp";
 
